fd27fe0dec728f5f73e2945c85e9c881147437a2
Jonathan Sanchez <jcsanchez@72-166-202-58.resnet.adams.edu>
2022-04-21 13:25:07 -0600

    Creating Connection to mysql and printing to the console

---
+++ ftmf.js
@@ -0,0 +1,11 @@
+'use strict'
+const inquirer = requirer('inquirer');
+const connection = requirer('./config/connection');
+
+requirer('console.table')
+
+const UniversityData = [];
+
+console.log(
+    Hello World
+);
